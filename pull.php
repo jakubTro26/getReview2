db2c979426eb7cb534a34f674a8dbb8387cec085
kuba <Jakub Troczynski>
2023-05-08 20:28:13 +0200

    commit

--- pull.php
+++ pull.php
@@ -12,9 +12,9 @@ error_reporting(E_ALL);
 
     
 //putenv('PATH=/usr/local/bin');
-$output = shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
+//$output = shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
 
-
+$output = shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>/dev/null >/dev/null &');
  echo "<pre>$output</pre>";
 
 //boftffg
