b6ad77cf78618616bb03a009b426eab8fa2338b2
kuba <Jakub Troczynski>
2023-05-08 20:18:32 +0200

    commit

--- pull.php
+++ pull.php
@@ -3,15 +3,15 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 //hellofff
 
-echo 'kuba1234';
-
-print_f( $_POST);
-if ( $_POST['payload'] ) {
-    echo 'mati123';
-//putenv('PATH=/usr/local/bin');
-//echo shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
 
 
-}
+
+
+    
+putenv('PATH=/usr/local/bin');
+echo shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
+
+
+
 //boftffg
 ?>
