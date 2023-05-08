497cff0b218231ed036ada8220fb5c357a9f91c2
kuba <Jakub Troczynski>
2023-05-08 20:41:27 +0200

    commit

--- pull.php
+++ pull.php
@@ -5,17 +5,11 @@
 
 
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 
-    
-//putenv('PATH=/usr/local/bin');
-//$output = shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
+echo shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>&1');
 
-$output = shell_exec('cd /var/www/u202319/data/www/woo.dmn.gg && /usr/bin/git pull origin main 2>/dev/null >/dev/null &');
- echo "<pre>$output</pre>";
+
 
 //boftffg
 ?>
